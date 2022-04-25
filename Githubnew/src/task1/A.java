2138397d3847ddc8d1232392b8d5c92a8defa25f
Vinay Pattiwar <vinay.pattiwar@gmail.com>
2022-04-25 12:16:42 +0530

    by jagruti1

--- Githubnew/src/task1/A.java
+++ Githubnew/src/task1/A.java
@@ -2,8 +2,15 @@ package task1;
 
 public class A {
 
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hi");
+		
+		A a=new A();
+		 a.m1();
 	}
 
 }
